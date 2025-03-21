86f29408b1fec4df15088e51dcfb09e4b15a91c8
Tobi Farinde <thetobifarinde@gmail.com>
2025-03-21 12:35:08 +0100

    Variables

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //console.log("hello world");
-c//onsole.log("I like pizza");
+//onsole.log("I like pizza");
 
 //window.alert("this is an alert");
 //window.alert("I like to fuck");
 //window.alert("i like to eat");
+
+//document.getElementById("myh1").textContent = "Hello";
+//document.getElementById("myp").textContent = "My world";
+
+let age = 25;
+let price =10.99;
+
+console.log(age);
+console.log(price)
