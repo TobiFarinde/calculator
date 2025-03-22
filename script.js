59938c5481cd1b651f5c1491439546b26458ea57
Tobi Farinde <thetobifarinde@gmail.com>
2025-03-22 23:42:06 +0100

    css layout

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-//console.log("hello world");
-//onsole.log("I like pizza");
 
-//window.alert("this is an alert");
-//window.alert("I like to fuck");
-//window.alert("i like to eat");
-
-//document.getElementById("myh1").textContent = "Hello";
-//document.getElementById("myp").textContent = "My world";
-
-let age = 25;
-let price =10.99;
-let gpa = 2.1;
-
-console.log("you are ${age} years old");
-console.log(price)
-console.log(gpa);
